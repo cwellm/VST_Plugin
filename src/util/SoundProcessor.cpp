a5de211c72146253ac2895388990a13f543d5b0f
Christoph Wellm <christoph.wellm@creaflect.de>
2023-04-03 15:46:22 +0200

    set up sound processor

---
+++ src/util/SoundProcessor.cpp
@@ -0,0 +1,5 @@
+#include "SoundProcessor.h"
+
+namespace cw::synth {
+    
+} // namespace cw::synth
---
+++ src/util/SoundProcessor.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace cw::synth {
+    
+class SoundProcessor {
+
+};
+
+} // namespace cw::synth
