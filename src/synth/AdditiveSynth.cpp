2c7febe331086f72102e859e4bdc1e9770851cc4
Christoph Wellm <christoph.wellm@creaflect.de>
2023-04-23 10:57:02 +0200

    Ensure reuse compliance - add all

--- src/synth/AdditiveSynth.cpp
+++ src/synth/AdditiveSynth.cpp
@@ -1,21 +1,20 @@
-/*
-  ==============================================================================
-
-   This file is part of the JUCE tutorials.
-   Copyright (c) 2020 - Raw Material Software Limited
-
-   The code included in this file is provided under the terms of the ISC license
-   http://www.isc.org/downloads/software-support-policy/isc-license. Permission
-   To use, copy, modify, and/or distribute this software for any purpose with or
-   without fee is hereby granted provided that the above copyright notice and
-   this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES,
-   WHETHER EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR
-   PURPOSE, ARE DISCLAIMED.
-
-  ==============================================================================
-*/
+/**
+ * Additive Synth - Experimental Synthesizer with some features to explore.
+ *
+ * Copyright (C) 2023 Christoph Wellm <christoph.wellm@creaflect.de>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License version 3 as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
 
 /*******************************************************************************
  The block below describes the properties of this PIP. A PIP is a short snippet
